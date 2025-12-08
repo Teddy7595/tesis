fb89e7f0a5a5a6d4252b30997eb43e42ea7732fd
Teddy Pottella <teddypottella@gmail.com>
2025-12-07 21:41:17 -0400

    feat: add EventEmitterModule to CoreModule for event handling

--- backend_js/src/core/core.module.ts
+++ backend_js/src/core/core.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
-  imports: [],
+  imports: [
+    EventEmitterModule.forRoot({
+      wildcard: true,
+      delimiter: '.',
+      maxListeners: 10,
+      verboseMemoryLeak: true,
+      ignoreErrors: false
+    })
+  ],
   controllers: [],
   providers: [],
   exports: []
 })
-export class CoreModule {}
+export class CoreModule { }
